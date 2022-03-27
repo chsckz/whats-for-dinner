e52086f04b2b19bd3b3094eb2db48f5989369a12
James Lee <lee.jamests@gmail.com>
2022-03-26 23:34:34 -0400

    Add new recipe function.

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 //Selectors
-const addRecipeBtn = document.getElementById('addRecipeBtn');
-const letsCookBtn = document.getElementById('letsCookBtn');
-const clearBtn = document.getElementById('clearBtn');
+const addARecipeBtn = document.querySelector('#addRecipeBtn');
+const letsCookBtn = document.querySelector('#letsCookBtn');
+const clearBtn = document.querySelector('#clearBtn');
+const addNewRecipeBtn = document.querySelector('#addNewRecipeBtn')
 
 const dishType = document.getElementsByTagName('dishType');
 const insertDish = document.getElementById('insertDish');
 const cookPotImg = document.querySelector('#cookPotImg');
 
 const mealContainer = document.querySelector('.mealContainer');
+
+
 
 //Recipe storage arrays
 let sideDishes = [
@@ -17,26 +20,26 @@ let sideDishes = [
   'Cambell\'s Soup',
   'Fingerling Potatoes',
   'A Single Carrot',
-  'Crimini Mushrooms with Escargot Butter'
+  'Crimini Mushrooms in Escargot Butter'
 ];
 let entree = [
-  '24oz Wagyu Ribeye - served with a wild berry & merlot reduction sauce',
+  '24oz Wagyu Ribeye with a wild berry & merlot reduc. sauce',
   'Cup Ramen',
   'Avocado Toast'
 ];
 let dessert = [
   'Skittles',
   'Chocolate volcano',
-  'Pint of Rocky Road Ice Cream',
+  'Rocky Road Ice Cream',
   'Mochi',
   'An Orange'
 ];
 let entireMeal; //random pick from 1 of each above array
 
 //Event listeners
-letsCookBtn.addEventListener('click', dishInputValue);
+letsCookBtn.addEventListener('click', typeOfDishToCook);
 clearBtn.addEventListener('click', clear);
-
+addNewRecipeBtn.addEventListener('click', addNewRecipe);
 
 
 //Functions
@@ -67,7 +70,7 @@ function suggestEntireMeal() {
   insertDish.textContent = `${tempEntree} with a side of ${tempSide} and ${tempDessert} for dessert!`
 }
 
-function dishInputValue() {
+function typeOfDishToCook() {
   let tempDishValue = document.querySelector('input[name="dishType"]:checked').value;
   mealContainer.classList.remove('hidden');
   cookPotImg.classList.add('hidden');
@@ -88,4 +91,12 @@ function dishInputValue() {
 function clear() {
   mealContainer.classList.add('hidden');
   cookPotImg.classList.remove('hidden')
+}
+
+function addNewRecipe() {
+  let dishType = document.querySelector('input[name="recipeType"]').value;
+  let dishName = document.querySelector('input[name="recipeName"]').value;
+  if (dishType == 'side' || 'dessert') {
+    alert('Please use a valid dish type');
+  }
 }
